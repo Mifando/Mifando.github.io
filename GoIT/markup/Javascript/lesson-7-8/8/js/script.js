2f3e47153a22194d20ec34597c301387cf087452
Mikhailo Krasnyi <mkrasnyy@gmail.com>
2016-02-18 15:53:03 +0200

    fixed lesson 8 + lessons 9-10

--- GoIT/markup/Javascript/lesson-7-8/8/js/script.js
+++ GoIT/markup/Javascript/lesson-7-8/8/js/script.js
@@ -1,62 +1,76 @@
-
-$(function() {
-	// $('<div></div>').appendTo('body').addClass('container');
-	// $('<form></form>').appendTo('.container').addClass('form-horizontal');
-	
-	// for (var i = 1; i < 4; i++) {
-	// 	$('<div></div>').appendTo('.form-horizontal').addClass('form-group');
-	// 	$('<label></label>').appendTo('.form-group:nth-child(' + i + ')').addClass('col-sm-2 control-label')
-	// 	.attr({
-	// 		'for':'text'+i}	
-	// 		);
-	// 	$('<div></div>').appendTo('.form-group:nth-child(' + i + ')').addClass('col-sm-10');
-	// 	$('<input></input>').appendTo('.form-group:nth-child(' + i + ')'+'> .col-sm-10').addClass('form-control')
-	// 	.attr({
-	// 		'type':'text',
-	// 		'id':'text'+i,
-	// 		'placeholder': 'Please enter this field'
-
-	// 	});
-	// };
-
-	// $('.form-group:first-child > label').html('Firstname');
-	// $('.form-group:nth-child(2) > label').html('Lastname');
-	// $('.form-group:nth-child(3) > label').html('Address');
-
-	// $('<button>Show all tooltips</button>').appendTo('form').addClass('btn btn-lg btn-default');
-	
-
-	var k = 0;	
-
-	$('button').on('click', (function(e){
-		e.preventDefault();
-		console.log($('span'));
-		if (k===0) {
-			for (var i = 1; i < 4; i++) {
-				$('.form-group:nth-child(' + i + ')'+'> .col-sm-10')
-				.append($('<span>Please enter ' + ($('.form-group:nth-child(' + i + ')').find('.form-control').attr('title')) + '</span>').addClass('tooltips'));
-				};
-		
-			k++;
-				} else {
-					$('span').remove();
-					k = 0;
-
-				};
-
-		}));
-	
-	$('.col-sm-10').hover(
-  		function() {
-  		$('span').remove();
-  		k=0;
-  	   	$(this).append($('<span>Please enter ' + ($(this).find('.form-control').attr('title')) + '</span>').addClass('tooltips'));
-    
-  		}, function() {
-    		$(this).find( 'span:last' ).remove();
-  		}
-	);
-
-
+$(function(){
+var title;
+var showOn = false;
+/*functions-----------------*/
+//fonction add himt
+	function addHint(id, name, child){
+		title = $(id).attr('title');
+		$(name).append('<span class = "hint"></span>');
+		var ch = $('.hint').addClass(child);
+		$(ch).text(title);
+		$(ch).animate({
+			opacity: 1
+		}, 500)
+	}
+//fonction add all hint
+	function addAllHint(){
+		showOn = true;
+		addHint('#firstName', '.boxForFirstName', 'fName');
+		addHint('#lastName', '.boxForLastName', 'lName');
+		addHint('#address', '.boxForAdrees', 'addr');
+	}
+//fonction delete hint	
+	function deleteHint(name){
+		$('.'+name).remove();
+	}
+//fonction delete all hint
+	function deleteAllHint(){
+		showOn = false;
+		$('.fName').remove();
+		$('.lName').remove();
+		$('.addr').remove();
+	}
+/*events--------------------*/
+//event add help first name
+	$('#firstName').on('mouseover', function(){
+		if(showOn)
+			deleteAllHint();
+		addHint('#firstName', '.boxForFirstName', 'fName')
+	})
+	//event delete help
+		$('#firstName').on('mouseout', function(){
+			deleteHint('fName');
+		})
+//event add help last name
+	$('#lastName').on('mouseover', function(){
+		if(showOn)
+			deleteAllHint();
+		addHint('#lastName', '.boxForLastName', 'lName')
+	})
+	//event delete help
+		$('#lastName').on('mouseout', function(){
+			deleteHint('lName');
+		})
+//event add help address
+	$('#address').on('mouseover', function(){
+		if(showOn)
+			deleteAllHint();
+		addHint('#address', '.boxForAdrees', 'addr')
+	})
+	//event delete help
+		$('#address').on('mouseout', function(){
+			deleteHint('addr');
+		})
+//opacity button show help
+	$('.showH').on('mouseover', function(){
+		$('.showH').css("opacity", "1");
+	})
+	$('.showH').on('mouseout', function(){
+		$('.showH').css("opacity", "0.5");
+	})
+//event hint all
+	$('.showH').on('click', function(){
+		if(!showOn)
+			addAllHint();
+		})
 });
-
